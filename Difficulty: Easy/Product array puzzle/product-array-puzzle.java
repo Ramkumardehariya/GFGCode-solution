753b6a8c89f1049a5a69c6d36a1abf73a79f87d7
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-01-17 18:33:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int[] productExceptSelf(int arr[]) {
+        // code here
+        int n = arr.length;
+        int ans[] = new int[n];
+        long res = 1;
+        boolean flag = false;
+        int zeroCount = 0;
+        
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == 0){
+                zeroCount++;
+                flag = true;
+                
+            }
+            else{
+                res = res*arr[i];
+            }
+            if(zeroCount > 1){
+                return ans;
+            }
+        }
+        
+        
+        
+        for(int i = 0; i < n; i++){
+            if(flag == true){
+                if(arr[i] != 0){
+                    ans[i] = 0;
+                }
+                else{
+                    ans[i] = (int)res;
+                }
+            }
+            else{
+                ans[i] = (int)res/arr[i];
+            }
+        }
+        
+        return ans;
+    }
+}
