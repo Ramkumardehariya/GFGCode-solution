ed3004687953e7375ef15e74431339ca1c327338
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-07-25 10:26:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
@@ -1,44 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
     pair<int, int> getMinMax(vector<int> arr) {
         // code here
+        pair<int,int> ans;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
         
-        sort(arr.begin(), arr.end());
+        for(int num: arr){
+            mini = min(mini, num);
+            maxi = max(maxi, num);
+        }
         
-        return {arr[0], arr[arr.size()-1]};
+        ans.first = mini;
+        ans.second = maxi;
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        pair<int, int> pp = ob.getMinMax(arr);
-        cout << pp.first << " " << pp.second << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
