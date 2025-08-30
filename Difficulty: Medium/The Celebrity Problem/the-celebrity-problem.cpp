87fe20a7d5e0aea532f5f53e6cd12dba00fbc285
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-08-30 10:15:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,52 +1,32 @@
 class Solution {
   public:
-    int celebrity(vector<vector<int> >& mat) {
+    int celebrity(vector<vector<int>>& mat) {
         // code here
-        stack<int> st;
-        
         int n = mat.size();
         
-        for(int i = 0; i<n; i++){
-            st.push(i);
-        }
-        
-        while(st.size() > 1){
-            int a = st.top();
-            st.pop();
-            
-            int b = st.top();
-            st.pop();
-            
-            if(mat[a][b] == 1){
-                st.push(b);
-            }
-            else{
-                st.push(a);
-            }
-        }
-        
-        int num = st.top();
-        st.pop();
-        bool rowCheck = true;
-        
-        for(int i = 0; i<n; i++){
-            if(i != num && mat[num][i] == 1){
-                rowCheck = false;
-            }
-        }
-        
-        bool colCheck = true;
-        
         for(int i = 0; i<n;i++){
-            if(i != num && mat[i][num] == 0){
-                colCheck = false;
+            int row = i;
+            int col = i;
+            
+            int zeroCount = 0;
+            int oneCount = 0;
+            
+            for(int j = 0; j<n; j++){
+                if(row != j && mat[row][j] == 0){
+                    zeroCount++;
+                }
+            }
+            
+            for(int j = 0; j<n; j++){
+                if(col != j && mat[j][col] == 1){
+                    oneCount++;
+                }
+            }
+            
+            if(zeroCount == n-1 && oneCount == n-1){
+                return i;
             }
         }
-        
-        if(rowCheck && colCheck){
-            return num;
-        }
-        
         return -1;
     }
 };
