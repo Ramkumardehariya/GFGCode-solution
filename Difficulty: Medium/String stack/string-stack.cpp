eb178de6f301548a2fc95a73bba5fa33539e966e
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-09-15 09:54:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/String stack/string-stack.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    bool stringStack(string &pat, string &tar) {
+        // code here
+        int n = pat.size();
+        int m = tar.size();
+        
+        int i = n-1;
+        int j = m-1;
+        
+        while(i >= 0 && j >= 0){
+            if(pat[i] == tar[j]){
+                i--;
+                j--;
+            }
+            else{
+                i -= 2;
+            }
+        }
+        
+        if(j < 0){
+            return true;
+        }
+        return false;
+    }
+};
