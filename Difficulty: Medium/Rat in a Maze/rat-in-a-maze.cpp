279d421ff8eac6f5f18053336a339cac07d9f2af
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-10-16 13:35:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.cpp
@@ -1,74 +1,61 @@
 class Solution {
   public:
-    bool isSafe(vector<vector<int>> &vis, int n, int m, vector<vector<int>> &maze, int x, int y){
-        if((x >= 0 && x <= n-1) && (y >= 0 && y <= m-1) && (vis[x][y] == 0) && maze[x][y] == 1){
+    bool isSafe(int x, int y, vector<vector<int>> &maze, vector<vector<int>> &vis){
+        if((x >= 0 && x < maze.size()) && (y >= 0 && y < maze[0].size()) && (vis[x][y] == 0) && maze[x][y] == 1){
             return true;
         }
         return false;
     }
-    void solve(vector<vector<int>> &maze,int n,int m, int x, int y, string path, vector<string> &ans,vector<vector<int>> &vis){
-        if(x == n-1 && y == m-1){
+    void solve(int x, int y,vector<vector<int>> &maze, vector<vector<int>> &vis, vector<string> &ans, string path){
+        if(x == maze.size()-1 && y == maze[0].size()-1){
             ans.push_back(path);
             return;
         }
         vis[x][y] = 1;
-        
-        //down
-        int newX = x+1;
-        int newY = y;
-        
-        if(isSafe(vis,n,m, maze, newX, newY)){
+        int newx = x+1;
+        int newy = y;
+        if(isSafe(newx, newy, maze, vis)){
             path.push_back('D');
-            solve(maze, n,m, newX, newY, path, ans, vis);
+            solve(newx, newy, maze, vis, ans, path);
             path.pop_back();
         }
         
-        //left
-        newX = x;
-        newY = y-1;
-        if(isSafe(vis,n,m, maze, newX, newY)){
+        newx = x;
+        newy = y-1;
+        if(isSafe(newx, newy, maze, vis)){
             path.push_back('L');
-            solve(maze, n,m, newX, newY, path, ans, vis);
+            solve(newx, newy, maze, vis, ans, path);
             path.pop_back();
         }
         
-        //right
-        newX = x;
-        newY = y+1;
-        if(isSafe(vis,n,m, maze, newX, newY)){
+        newx = x;
+        newy = y+1;
+        if(isSafe(newx, newy, maze, vis)){
             path.push_back('R');
-            solve(maze, n,m, newX, newY, path, ans, vis);
+            solve(newx, newy, maze, vis, ans, path);
             path.pop_back();
         }
-        //up
-        newX = x-1;
-        newY = y;
         
-        if(isSafe(vis,n,m ,maze, newX, newY)){
+        newx = x-1;
+        newy = y;
+        if(isSafe(newx, newy, maze, vis)){
             path.push_back('U');
-            solve(maze, n,m, newX, newY, path, ans, vis);
+            solve(newx, newy, maze, vis, ans, path);
             path.pop_back();
         }
-        vis[x][y] = 0;
         
+        
+        vis[x][y] = 0;
     }
     vector<string> ratInMaze(vector<vector<int>>& maze) {
-        // code here
         vector<string> ans;
         int n = maze.size();
         int m = maze[0].size();
         
-        vector<vector<int>> vis= maze;
+        vector<vector<int>> vis(n,vector<int>(m, 0));
+
+        solve(0, 0,maze, vis, ans, "");
         
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n;j++){
-                vis[i][j] = 0;
-            }
-        }
-        
-        string path = "";
-        
-        solve(maze,n,m, 0, 0,path, ans, vis);
         return ans;
     }
 };
