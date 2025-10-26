2de10183e8327f2b8b5cb2c049642ca83a7d645e
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-10-26 12:52:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
+++ Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -2,24 +2,20 @@ class Solution {
   public:
     int minCost(vector<int>& arr) {
         // code here
-        priority_queue<int, vector<int>,greater<int>> pq;
+        priority_queue<int, vector<int>, greater<int>> mnHeap;
+        int n = arr.size();
+        for (int i=0; i<n; i++) mnHeap.push(arr[i]);
         
-        for(int i = 0; i<arr.size(); i++){
-            pq.push(arr[i]);
-        }
-        int ans =0;
-        
-        while(pq.size() > 1){
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
-            pq.pop();
+        int ans = 0;
+        while (mnHeap.size()>1) {
+            int t1 = mnHeap.top();
+            mnHeap.pop();
+            int t2 = mnHeap.top();
+            mnHeap.pop();
             
-            int sum = a+b;
-            ans += sum;
-            pq.push(sum);
+            ans += (t1+t2);
+            mnHeap.push(t1+t2);
         }
-        
         return ans;
     }
 };
