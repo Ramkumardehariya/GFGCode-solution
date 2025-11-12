64375eb1be11cded185661535c991e355bd9129a
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-11-12 16:37:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Get Minimum Squares/get-minimum-squares.cpp
+++ Difficulty: Medium/Get Minimum Squares/get-minimum-squares.cpp
@@ -1,37 +1,25 @@
 class Solution {
   public:
   
-  int f(int i,int sum,vector<vector<int>> &dp,vector<int> &v){
-      if(sum==0){
-          return 0;
-      }
-      if(i>=v.size()){
-          return 1e5;
-      }
-      if(dp[i][sum]!=-1){
-          return dp[i][sum];
-      }
-      if(v[i]<=sum){
-          return dp[i][sum]=min(1+f(i,sum-v[i],dp,v),f(i+1,sum,dp,v));
-      }else{
-          return dp[i][sum]=f(i+1,sum,dp,v);
-      }
-  }
+    int solve(int n, vector<int> &dp){
+        if(n == 0){
+            return 0;
+        }
+        
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        int ans = INT_MAX;
+        for(int i = 1; i*i<=n; i++){
+            ans = min(ans, 1+solve(n-i*i, dp));
+        }
+        
+        return dp[n] = ans;
+    }
     int minSquares(int n) {
         // Code here
-        int temp=n;
-        // int count=0;
-        vector<int> v;
-        for(int i=n;i>=1;i--){
-            if(i<=temp){
-                int a=sqrt(i);
-                if(a*a==i){
-                  v.push_back(i);
-                }
-            }
-        }
-        // cout<<count;
-        vector<vector<int>> dp(v.size(),vector<int>(n+1,-1));
-        return f(0,temp,dp,v);
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
     }
 };
