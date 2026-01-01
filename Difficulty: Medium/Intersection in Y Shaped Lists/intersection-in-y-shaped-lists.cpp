9f3b9a82aa5d1853812f5464c7bb985e72bf006f
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2026-01-01 12:16:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        //  Code Here
+        unordered_map<Node*,bool>mp;
+        Node * travel = head1;
+        while(travel){
+            mp[travel] = true;
+            travel=travel->next;
+        }
+        travel = head2;
+        while(travel){
+            if(mp[travel]) return travel;
+            travel=travel->next;
+        }
+    }
+};
