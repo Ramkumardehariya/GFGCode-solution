56ea3cf2777f08bc88d18b363d0a8812a5edbfac
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2026-02-12 12:27:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max rectangle/max-rectangle.cpp
+++ Difficulty: Hard/Max rectangle/max-rectangle.cpp
@@ -1,92 +1,78 @@
 class Solution {
   public:
-    vector<int> prevSmallerElement(vector<int> &prev, int n){
-        vector<int> ans(n);
-        int j= 0;
-        
+  
+    void prevSmaller(vector<int> &prev, vector<int> &arr){
         stack<int> st;
         st.push(-1);
         
-        for(int i = 0; i<n; i++){
-            int curr = prev[i];
-            
-            while(st.top() != -1 && prev[st.top()] >= curr){
+        for(int i = 0; i<arr.size(); i++){
+            int curr = arr[i];
+            while(st.top() != -1 && arr[st.top()] >= curr){
                 st.pop();
             }
-            ans[j] = st.top();
-            j++;
+            prev[i] = st.top();
             st.push(i);
+        }
+    }
+    
+    void nextSmaller(vector<int> &next, vector<int> &arr){
+        stack<int> st;
+        st.push(-1);
+        
+        for(int i = arr.size()-1; i>=0; i--){
+            int curr = arr[i];
+            while(st.top() != -1 && arr[st.top()] >= curr){
+                st.pop();
+            }
+            next[i] = st.top();
+            st.push(i);
+        }
+    }
+    
+    int largestArea(vector<int> &arr){
+        int n = arr.size();
+        vector<int> prev(n,0);
+        vector<int> next(n,0);
+        
+        prevSmaller(prev, arr);
+        nextSmaller(next, arr);
+        int ans = 0;
+        
+        for(int i = 0; i<n; i++){
+            int l = arr[i];
+            if(next[i] == -1){
+                next[i] = n;
+            }
+            int b = next[i]-prev[i]-1;
+            
+            int area = l*b;
+            
+            ans = max(ans, area);
         }
         return ans;
     }
     
-    vector<int> nextSmallerElement(vector<int> &prev, int n){
-        vector<int> ans(n);
-        int j= n-1;
-        
-        stack<int> st;
-        st.push(-1);
-        
-        for(int i = n-1; i>=0; i--){
-            int curr = prev[i];
-            
-            while(st.top() != -1 && prev[st.top()] >= curr){
-                st.pop();
-            }
-            ans[j] = st.top();
-            j--;
-            st.push(i);
-        }
-        return ans;
-    }
-  
-    int histoGram(vector<int> &arr, int n){
-        int ans = 0;
-        
-        vector<int> next(n);
-        next = nextSmallerElement(arr, n);
-        
-        vector<int> prev(n);
-        prev = prevSmallerElement(arr, n);
-        
-        for(int i = 0; i<n; i++){
-            int l = arr[i];
-            
-            if(next[i] == -1){
-                next[i] = n;
-            }
-            int w = next[i]-prev[i]-1;
-            ans = max(ans, l*w);
-        }
-        
-        return ans;
-    }
     int maxArea(vector<vector<int>> &mat) {
-        vector<int> temp;
+        // code here
         int n = mat.size();
         int m = mat[0].size();
         
-        for(int j = 0; j<m; j++){
-            temp.push_back(mat[0][j]);
-        }
-        
-        int area = histoGram(temp, m);
-        
-        int ans = area;
+        int ans = 0;
         
         for(int i = 1; i<n; i++){
-            vector<int> temp2;
             for(int j = 0; j<m; j++){
-                if(mat[i][j] != 0){
-                    temp2.push_back(mat[i][j]+temp[j]);
+                if(mat[i][j] == 0){
+                    continue;
                 }
                 else{
-                    temp2.push_back(0);
+                    mat[i][j] = mat[i-1][j]+1;
                 }
             }
-            int area = histoGram(temp2, m);
+        }
+        
+        for(int i = 0; i<n; i++){
+            int area = largestArea(mat[i]);
             ans = max(ans, area);
-            temp = temp2;
         }
         return ans;
     }
