ed49849cd7245e278efd00072ef7af4e595b42d8
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2026-02-15 09:08:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -2,22 +2,26 @@ class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost) {
         //  code here
+        int deficit = 0;
         int start = 0;
         int balance = 0;
-        int deficit = 0;
         int n = gas.size();
         
-        for(int i = 0; i<gas.size(); i++){
-            balance += (gas[i]-cost[i]);
+        for(int i = 0; i<n; i++){
+            balance += gas[i]-cost[i];
+            
             if(balance < 0){
                 deficit += balance;
                 balance = 0;
                 start = i+1;
             }
         }
-        if(deficit+balance < 0){
-            return -1;
+        
+        if(deficit + balance >= 0){
+            return start;
         }
-        return start;
+        
+        
+        return -1;
     }
 };
